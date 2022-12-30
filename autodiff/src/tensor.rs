f573eb8b33c55c9c463b8945c1285fc4ca28d98e
noelfranthomas <noelfranthomas@gmail.com>
2022-12-30 13:31:16 -0700

    init tensor obj

---
+++ autodiff/src/tensor.rs
@@ -0,0 +1,94 @@
+use std::ops::{Add, Sub, Mul, Div};
+
+pub struct Node<T, U> {
+    data: T,
+    grad: U, // Should this always be f64?
+    
+    // graph construction
+    _backward: String, // ??
+    _prev: u64,// hashmap? hashset?
+    _op: String
+}
+
+impl<T, U> Node<T, U>{
+    pub fn new(data: T, grad: U) -> Self {
+        Self {
+            data,
+            grad,
+            _backward: String::from(""),
+            _prev: 1,// hashmap? hashset?
+            _op: String::from(""),
+        }
+    }
+}
+
+impl<T: Add<Output = T>, U> Add for Node<T, U> {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self {
+            data: self.data + other.data,
+            grad: self.grad, // Can be propogated as result's grad
+
+            _backward: String::from(""),
+            _prev: 1,// hashmap? hashset?
+            _op: String::from("+"),
+        }
+    }
+}
+
+impl<T: Sub<Output = T>, U> Sub for Node<T, U> {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self {
+            data: self.data - other.data,
+            grad: self.grad, // Can be propogated as result's grad
+
+            _backward: String::from(""),
+            _prev: 1,// hashmap? hashset?
+            _op: String::from("+"),
+        }
+    }
+}
+
+// Doesn't work for tensors
+impl<T: Mul<Output = T>, U> Mul for Node<T, U> {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self::Output {
+        Self {
+            data: self.data * other.data,
+            grad: self.grad,
+
+            _backward: String::from(""),
+            _prev: 1,// hashmap? hashset?
+            _op: String::from("+"),
+        }
+    }
+}
+
+impl<T: Div<Output = T>, U> Div for Node<T, U> {
+    type Output = Self;
+
+    fn div(self, other: Self) -> Self::Output {
+        Self {
+            data: self.data / other.data,
+            grad: self.grad, // Can be propogated as result's grad
+
+            _backward: String::from(""),
+            _prev: 1,// hashmap? hashset?
+            _op: String::from("+"),
+        }
+    }
+}
+
+// Should this be a trait/impl?
+fn print_type<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
+// Trace the graph
+// fn trace(root: &Value) {
+//     // Find all nodes
+// }
